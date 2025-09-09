15f29c67b4592459e558e4d273b418be69af1d2f
Jordan <hello@jordanrobinson.org>
2025-09-09 23:55:16 +0100

    change smtp settings

--- mataroa/settings.py
+++ mataroa/settings.py
@@ -181,8 +181,8 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 if LOCALDEV:
     EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 EMAIL_USE_TLS = True
-EMAIL_HOST = "smtp.postmarkapp.com"
-EMAIL_HOST_BROADCASTS = "smtp-broadcasts.postmarkapp.com"
+EMAIL_HOST = "smtp.fastmail.com"
+EMAIL_HOST_BROADCASTS = ""
 EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
 EMAIL_PORT = 587
