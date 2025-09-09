477acbb779093450d7948c83a81fa8381b676de1
Jordan <hello@jordanrobinson.org>
2025-09-09 23:26:37 +0100

    remove middleware for subdomain

--- mataroa/settings.py
+++ mataroa/settings.py
@@ -66,7 +66,6 @@ MIDDLEWARE = [
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
-    'mataroa.middleware.SubdomainMiddleware',
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
